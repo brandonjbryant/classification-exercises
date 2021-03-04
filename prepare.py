495c4d86a9b206a6f4c4d00ed77bd105dba71250
“Brandon <brandon.j.bryant1@gmail.com>
2021-03-04 14:32:50 -0600

    prepare.py

--- prepare.py
+++ prepare.py
@@ -34,3 +34,20 @@ def train_validate_test_split(df, seed=123):
         stratify=train_and_validate.survived,
     )
     return train, validate, test
+
+
+
+def split(df, stratify_by=None):
+    """
+    Crude train, validate, test split
+    To stratify, send in a column name
+    """
+    
+    if stratify_by == None:
+        train, test = train_test_split(df, test_size=.2, random_state=123)
+        train, validate = train_test_split(train, test_size=.3, random_state=123)
+    else:
+        train, test = train_test_split(df, test_size=.2, random_state=123, stratify=df[stratify_by])
+        train, validate = train_test_split(train, test_size=.3, random_state=123, stratify=train[stratify_by])
+    
+    return train, validate, test
